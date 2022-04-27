5051d1bb550407f6f8ecdba58855353c0b1e5408
alixepstein <97540392+alixepstein@users.noreply.github.com>
2022-04-27 11:40:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -326,4 +326,4 @@ why_move = df['d8b_moving_why'].dropna().tolist()
 overall_happiness = alt.Chart(df).mark_bar(size = 30).encode(
     alt.X('1_happy_now'),
     y='count()')
-st.altair_chart(overall_happiness)
+st.altair_chart(overall_happiness, use_container_width=False)
