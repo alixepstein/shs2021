92e7ab8dc06e05a752d26495a25f18aee214c49d
alixepstein <97540392+alixepstein@users.noreply.github.com>
2022-04-27 12:25:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ df = pd.read_csv('SHSdf.csv')
 
 
 #overall happiness
-overall_happiness = alt.Chart(df).mark_bar().encode(
+overall_happiness = alt.Chart(df).mark_circle().encode(
     alt.X('1_happy_now:Q'),
     alt.Y('count():Q'))
 st.altair_chart(overall_happiness, use_container_width=False)
