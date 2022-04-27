519a393388adb289149791e6a1b6ffe997d1917a
alixepstein <97540392+alixepstein@users.noreply.github.com>
2022-04-27 13:37:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,10 @@ import altair as alt
 import streamlit as st
 
 df = pd.read_csv('SHSdf.csv')
-df_sample = df[['1_happy_now', 'Ward']].head(20)
 
 
 #overall happiness
-overall_happiness = alt.Chart(df_sample).mark_bar().encode(
+overall_happiness = alt.Chart(df).mark_bar(size = 30).encode(
     alt.X('1_happy_now:Q'),
     alt.Y('count():Q'))
 st.altair_chart(overall_happiness, use_container_width=False)
