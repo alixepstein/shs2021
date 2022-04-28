0021f18757b1e125a57250374f61c9c0477f8f89
alixepstein <97540392+alixepstein@users.noreply.github.com>
2022-04-27 20:17:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,32 @@ st.set_page_config(
 st.title('Somerville Happiness Survey 2021')
 
 
+
+
+#CHARTS
+
+#overall happiness
+overall_happiness = alt.Chart(df).mark_bar(size = 30).encode(alt.X('1_happy_now:Q'), alt.Y('count():Q'))
+
+
+
+
+
+
+navigation = st.sidebar.radio("Explore the data by:", ('Overall trends', 'Gender','Age', 'Income')) 
+
+if navigation == 'Overall trends':
+    st.altair_chart(overall_happiness, use_container_width=True)
+elif navigation == 'Gender': 
+    #other things
+elif navigation == 'Age':
+    #other other things
+elif navigation == 'Income':
+    #other
+else:
+    #thing
+    
+
 #overall happiness
 overall_happiness = alt.Chart(df).mark_bar(size = 30).encode(
     alt.X('1_happy_now:Q'),
