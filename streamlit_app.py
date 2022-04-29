bf63835193428a9d000d0f83f198463bebc5fbd3
alixepstein <97540392+alixepstein@users.noreply.github.com>
2022-04-28 20:43:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,24 +83,24 @@ df_ward = df[df['Ward'] != 'No Answer Given']
 #rating of housing cost by ward
 ward_housing_cost = alt.Chart(df_ward).mark_bar().encode(
     alt.X('Ward:N', axis=alt.Axis(format='d')),
-    alt.Y('avg_housing_cost_ward:Q', title = 'rating of housing cost')
+    alt.Y('avg_housing_cost_ward:Q', title = 'rating of housing cost', axis=alt.Axis(format='d'))
 ).properties(title = 'Rating of housing cost by ward')
 
 #rating of maintenance of streets and sidewalks by ward
 ward_streets_sidewalks = alt.Chart(df_ward).mark_bar().encode(
-    alt.X('Ward:N'),
+    alt.X('Ward:N', axis=alt.Axis(format='d')),
     alt.Y('avg_streets_sidewalks_ward:Q', title = 'rating of maintenance of streets/sidewalks')
 ).properties(title = 'Rating of maintenance of streets/sidewalks by ward')
 
 #rating of feeling of safety when walking at night by ward
 ward_safe_night = alt.Chart(df_ward).mark_bar().encode(
-    alt.X('Ward:N'),
+    alt.X('Ward:N', axis=alt.Axis(format='d')),
     alt.Y('avg_safe_night_ward:Q', title = 'How safe do you feel at night?')
 ).properties(title = 'How safe people feel at night by ward')
 
 #rating of beauty of neighborhood by ward
 ward_beauty = alt.Chart(df_ward).mark_bar().encode(
-    alt.X('Ward:N'),
+    alt.X('Ward:N', axis=alt.Axis(format='d')),
     alt.Y('avg_neighborhood_beauty_ward:Q', title = 'Rating of neighborhood beauty')
 ).properties(title = 'Rating of neighborhood beauty by ward')
 
