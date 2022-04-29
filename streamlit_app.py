808477ef9f629cd619c5f04e34f70734bb46c36e
alixepstein <97540392+alixepstein@users.noreply.github.com>
2022-04-29 09:38:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,6 @@ ward_housing_cost = alt.Chart(df_ward).mark_bar().encode(
 ).properties(title = 'Rating of housing cost by ward')
 
 #rating of maintenance of streets and sidewalks by ward
-
 ward_streets_sidewalks = alt.Chart(df_ward).mark_bar().encode(
     alt.X('Ward:N', axis=alt.Axis(format='d')),
     alt.Y('avg_streets_sidewalks_ward', title = 'rating of maintenance of streets/sidewalks')
