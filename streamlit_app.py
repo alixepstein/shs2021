d12de0a7c0e761b433c582883a5270f2c13ea0a1
alixepstein <97540392+alixepstein@users.noreply.github.com>
2022-04-30 18:40:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -317,6 +317,171 @@ moving_satis = alt.Chart(df_moving_satis).mark_bar().encode(
     alt.X('Satisfaction:Q', title = 'Satisfaction', scale = alt.Scale(domain = (0,8))),
     alt.Y('Planning to Move:N'))
 
+# rent or own by income
+income_rent_own = [['$200,000 or more', 'Own', 0.7388888888888889],
+    ['$200,000 or more', 'Rent', 0.2611111111111111],
+    ['$150,000 to $199,999', 'Own', 0.4460431654676259],
+    ['$150,000 to $199,999', 'Rent', 0.5539568345323741],
+    ['$100,000 to $149,999', 'Own', 0.5],
+    ['$100,000 to $149,999', 'Rent', 0.5],
+    ['$75,000 to $99,999', 'Own', 0.43846153846153846],
+    ['$75,000 to $99,999', 'Rent', 0.5615384615384615],
+    ['$50,000 to $74,999', 'Own', 29/(29+80)],
+    ['$50,000 to $74,999', 'Rent', 80/(29+80)],
+    ['$25,000 to $49,999', 'Own', 28/(28+48)],
+    ['$25,000 to $49,999', 'Rent', 0.631578947368421],
+    ['$10,000 to $24,999', 'Own', 0.2807017543859649],
+    ['$10,000 to $24,999', 'Rent', 0.7192982456140351],
+    ['Less than $10,000', 'Own', 0.1935483870967742],
+    ['Less than $10,000', 'Rent', 0.8064516129032258]]
+df_income_rent = pd.DataFrame(income_rent_own,columns=['Income', 'Housing Status', 'Total'])
+income_order = ['Less than $10,000', '$10,000 to $24,999', '$25,000 to $49,999', '$50,000 to $74,999',
+    '$75,000 to $99,999', '$100,000 to $149,999', '$150,000 to $199,999', '$200,000 or more']
+rent_income = alt.Chart(df_income_rent).mark_bar().encode(
+    alt.X('Income:N', sort = income_order),
+    alt.Y('Total:Q', title = 'Percentage', axis=alt.Axis(format='%')),
+    color = alt.Color('Housing Status:N', sort = ['Own', 'Rent'], legend = alt.Legend(title = 'Rent or own')))
+
+#rent or own by race
+#percentage by race = 
+df_rent = df[df['d7_rent_own'] != 'No Answer Given']
+rro = df_rent.groupby('d4_race')['d7_rent_own'].value_counts(normalize = True)
+race_rent_own = [['Asian', 'Rent', rro[0]],
+    ['Asian', 'Own', rro[1]],
+    ['Black/African American', 'Rent', rro[2]],
+    ['Black/African American', 'Own', rro[3]],
+    ['Hispanic/Latino', 'Rent', rro[4]],
+    ['Hispanic/Latino', 'Own', rro[5]],
+    ['White', 'Rent', rro[11]],
+    ['White', 'Own', rro[12]]]
+df_race_rent = pd.DataFrame(race_rent_own,columns=['Race', 'Housing Status', 'Percentage'])
+rent_race = alt.Chart(df_race_rent).mark_bar().encode(
+    alt.X('Race:N', sort = ['White', 'Asian', 'Black/African American', 'Hispanic/Latino']),
+    alt.Y('Percentage:Q', title = 'Percentage', axis=alt.Axis(format='%')),
+    color = alt.Color('Housing Status:N', sort = ['Own', 'Rent'], legend = alt.Legend(title = 'Rent or own')))
+
+#plans to move by race
+race_moving = [['Asian', 'No', 0.656716],
+    ['Asian', 'Yes', 0.343284],
+    ['Black/African American', 'No', 0.612903],
+    ['Black/African American', 'Yes', 0.387097],
+    ['Hispanic/Latino', 'No', 0.580000],
+    ['Hispanic/Latino', 'Yes', 0.420000],
+    ['White', 'No', 0.648951],
+    ['White', 'Yes', 0.351049]]
+df_race_moving = pd.DataFrame(race_moving,columns=['Race', 'Plan to Move Yes/No', 'Percentage'])
+move_race = alt.Chart(df_race_moving).mark_bar().encode(
+    alt.X('Race:N', sort = ['Asian', 'White', 'Black/African American', 'Hispanic/Latino']),
+    alt.Y('Percentage:Q', title = 'Percentage', axis=alt.Axis(format='%')),
+    color = alt.Color('Plan to Move Yes/No:N', legend = alt.Legend(title = 'Plans to move')))
+
+#plans to move by whether or not you live with children under 18
+children_moving = [
+    ['No children', 'No', 0.620081],
+    ['No children', 'Yes', 0.379919],
+    ['Yes children', 'No', 0.704918],
+    ['Yes children', 'Yes', 0.295082]]
+df_children_moving = pd.DataFrame(children_moving,columns=['Children yes/no', 'Plan to Move Yes/No', 'Percentage'])
+move_children = alt.Chart(df_children_moving).mark_bar().encode(
+    alt.X('Children yes/no:N'),
+    alt.Y('Percentage:Q', title = 'Percentage', axis=alt.Axis(format='%')),
+    color = alt.Color('Plan to Move Yes/No:N', legend = alt.Legend(title = 'Plans to move')))
+
+#rent or own by age
+df_num = df[df['d2_age'] != 'No Age Given']
+df_num['d2_age'] = pd.to_numeric(df_num['d2_age'])
+age_bins = df_num.groupby(['d7_rent_own', pd.cut(df_num['d2_age'], [0, 10, 20, 30, 40, 50, 60, 70, 80, 90])])
+age_bins.size().unstack()
+age_rent_own = [
+    ['10', 'Own', .5],
+    ['10', 'Rent', .5],
+    ['20', 'Own', 20/(189)],
+    ['20', 'Rent', 169/189],
+    ['30', 'Own', 92/(92+167)],
+    ['30', 'Rent', 167/(92+167)],
+    ['40', 'Own', 96/(96+49)],
+    ['40', 'Rent', 49/(96+49)],
+    ['50', 'Own', 88/(88+34)],
+    ['50', 'Rent', 34/(88+34)],
+    ['60', 'Own', 83/(83+40)],
+    ['60', 'Rent', 40/(83+40)],
+    ['70', 'Own', 45/(45+18)],
+    ['70', 'Rent', 18/(45+18)],
+    ['80', 'Own', 10/15],
+    ['80', 'Rent', 5/15]]
+df_age_rent = pd.DataFrame(age_rent_own,columns=['Age', 'Housing Status', 'Percentage'])
+rent_age = alt.Chart(df_age_rent).mark_bar().encode(
+    alt.X('Age:N'),
+    alt.Y('Percentage:Q', title = 'Percentage', axis=alt.Axis(format='%')),
+    color = alt.Color('Housing Status:N', legend = alt.Legend(title = 'Rent or own')))
+
+#plans to move by age
+age_bins = df_num.groupby(['d8a_moving', pd.cut(df_num['d2_age'], [0, 10, 20, 30, 40, 50, 60, 70, 80, 90])])
+age_bins.size().unstack()
+age_moving = [
+    ['10', 'Yes', .5],
+    ['10', 'No', .5],
+    ['20', 'Yes', 100/188],
+    ['20', 'No', 88/(188)],
+    ['30', 'Yes', 122/(133+122)],
+    ['30', 'No', 133/(133+122)],
+    ['40', 'Yes', 38/(101+38)],
+    ['40', 'No', 101/(101+38)],
+    ['50', 'Yes', 28/(92+28)],
+    ['50', 'No', 92/(92+28)],
+    ['60', 'Yes', 25/(89+25)],
+    ['60', 'No', 89/(89+25)],
+    ['70', 'Yes', 8/(57+8)],
+    ['70', 'No', 57/(57+8)],
+    ['80', 'Yes', 2/15],
+    ['80', 'No', 13/15],]
+df_age_moving = pd.DataFrame(age_moving,columns=['Age', 'Plans to Move', 'Percentage'])
+move_age = alt.Chart(df_age_moving).mark_bar().encode(
+    alt.X('Age:N'),
+    alt.Y('Percentage:Q', title = 'Percentage', axis=alt.Axis(format='%')),
+    color = alt.Color('Plans to Move:N', legend = alt.Legend(title = 'Plans to Move')))
+
+#plans to move by income
+income_moving = [
+    ['$200,000 or more', 'No', 0.744318],
+    ['$200,000 or more', 'Yes', 0.255682],
+    ['$150,000 to $199,999', 'No', 0.514493],
+    ['$150,000 to $199,999', 'Yes', 0.485507],
+    ['$100,000 to $149,999', 'No', 0.573333],
+    ['$100,000 to $149,999', 'Yes', 0.426667],
+    ['$75,000 to $99,999', 'No', 0.612903],
+    ['$75,000 to $99,999', 'Yes', 0.387097],
+    ['$50,000 to $74,999', 'No', 0.571429],
+    ['$50,000 to $74,999', 'Yes', 0.428571],
+    ['$25,000 to $49,999', 'No', 0.697368],
+    ['$25,000 to $49,999', 'Yes', 0.302632],
+    ['$10,000 to $24,999', 'No', 0.727273],
+    ['$10,000 to $24,999', 'Yes', 0.272727],
+    ['Less than $10,000', 'No', 0.657143],
+    ['Less than $10,000', 'Yes', 0.342857]]
+df_income_moving = pd.DataFrame(income_moving ,columns=['Income', 'Plans to Move', 'Percentage'])
+move_income = alt.Chart(df_income_moving).mark_bar().encode(
+    alt.X('Income:N', sort = income_order),
+    alt.Y('Percentage:Q', title = 'Percentage', axis=alt.Axis(format='%')),
+    color = alt.Color('Plans to Move:N', legend = alt.Legend(title = 'Plans to Move')))
+
+#rent or own by survey language
+df.groupby('survey_language')['d7_rent_own'].value_counts()
+language_rent_own = [
+    ['English', 'Own', 438/(438+464)],
+    ['English', 'Rent', 464/(438+464)],
+    ['Portuguese', 'Own', 3/11],
+    ['Portuguese', 'Rent', 8/11],
+    ['Spanish', 'Own', 7/24],
+    ['Spanish', 'Rent', 17/24]]
+df_language_rent = pd.DataFrame(language_rent_own,columns=['Language', 'Housing Status', 'Percentage'])
+rent_language = alt.Chart(df_language_rent).mark_bar().encode(
+    alt.X('Language:N', sort = ['English', 'Spanish', 'Portuguese']),
+    alt.Y('Percentage:Q', title = 'Percentage', axis=alt.Axis(format='%')),
+    color = alt.Color('Housing Status:N', legend = alt.Legend(title = 'Rent or own')))
+
+
+
 
 
 
