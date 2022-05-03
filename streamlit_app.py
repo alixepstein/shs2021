73de383963865bd00da7a5563941d71f414e4c05
alixepstein <97540392+alixepstein@users.noreply.github.com>
2022-05-02 21:10:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ total_averages = [
 df_total_averages = pd.DataFrame(total_averages,columns=['Question','Rating'])
 total_averages_chart = alt.Chart(df_total_averages).mark_bar().encode(
     alt.X('Rating:Q', scale=alt.Scale(domain=(0, 1))),
-    alt.Y('Question:N', sort = total_average_order)).properties(title = 'Average ratings')
+    alt.Y('Question:N', sort = total_average_order)).properties(title = 'Average ratings', width = 700)
 
 #AGE
 
